002a185ab661d603823b9cd73b97acad13bfeb16
Noname <noemail@noemail.noemail>
2025-02-05 15:03:00 +0300

    Simple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <GLFW/glfw3.h>
+
 int main() {
+    glfwInit();
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "TheVeryBestGayme", nullptr, nullptr);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
