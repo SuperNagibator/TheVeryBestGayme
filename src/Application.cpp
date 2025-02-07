ecd455a9dcdbef9bd9f88dddcb579e94ee13cb7b
SuperNagibator <124372185+SuperNagibator@users.noreply.github.com>
2025-02-07 14:24:06 +0300

    Simple rectangle

--- src/Application.cpp
+++ src/Application.cpp
@@ -42,13 +42,15 @@ Application::Application() {
     frag.push_back('\0');
 
     std::vector<float> vertices = {
-        0.0, 0.0,
-        1.0, 0.0,
-        0.0, 1.0,
+        -0.5, -0.5,
+        0.5, -0.5,
+        -0.5, 0.5,
+        0.5, 0.5,
     };
 
     std::vector<unsigned short> triangles = {
-        0, 1, 2
+        0, 1, 2,
+        3, 2, 1,
     };
 
     std::shared_ptr<Shader> shader = std::make_shared<Shader>(vert.data(), frag.data());
